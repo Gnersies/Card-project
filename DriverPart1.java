0a4b523ad92b17984e5d21970de1f9643e52512c
Gnersies <justin.morgan99@yahoo.com>
2016-12-16 12:42:21 -0700

    .

---
+++ DriverPart1.java
@@ -0,0 +1,10 @@
+public class DriverPart1 {
+	public static void main(String[] args) {
+		DeckOfCardsPart1 deck = new DeckOfCardsPart1();
+		deck.shuffle();
+		for (int n = 1; n <= 5; n++){
+			System.out.println(deck.deal(n).number() + " of " + deck.deal(n).suit());
+		}
+	}
+
+}
